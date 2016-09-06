68458c572c73ab72ded6b32e2c9d7feb6608aee4
wangjian <wangjian@hyxt.com>
2016-09-06 16:12:21 +0800

    首次提交

---
+++ app/src/main/java/com/jarek/titleview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jarek.titleview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+
+ * Created by Jarek on 2016/9/6.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
